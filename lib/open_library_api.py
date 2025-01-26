86f0b23f9b325c54cb9b18ae2decf667655958fa
Stuart Forrester <stforrester@gmail.com>
2025-01-25 22:32:17 -0600

    replaces LOTR with 'The Power of One'

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -5,7 +5,7 @@ import json
 class Search:
 
     def get_search_results(self):
-        search_term = "the lord of the rings"
+        search_term = "the power of one"
 
         search_term_formatted = search_term.replace(" ", "+")
         fields = ["title", "author_name"]
@@ -20,7 +20,7 @@ class Search:
         return response.content
 
     def get_search_results_json(self):
-        search_term = "the lord of the rings"
+        search_term = "the power of one"
 
         search_term_formatted = search_term.replace(" ", "+")
         fields = ["title", "author_name"]
@@ -39,6 +39,7 @@ class Search:
         limit = 1
 
         URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+        print(URL)
 
         response = requests.get(URL).json()
         response_formatted = f"Title: {response['docs'][0]['title']}\nAuthor: {response['docs'][0]['author_name'][0]}"
@@ -48,8 +49,8 @@ class Search:
 # results = Search().get_search_results()
 # print(results)
 
-results_json = Search().get_search_results_json()
-print(json.dumps(results_json, indent=1))
+# results_json = Search().get_search_results_json()
+# print(json.dumps(results_json, indent=1))
 
 # search_term = input("Enter a book title: ")
 # result = Search().get_user_search_results(search_term)
